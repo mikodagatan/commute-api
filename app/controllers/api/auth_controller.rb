52df0688ae3713c5f7b749928e1e66d9db39e840
Miko Dagatan <miguel.dagatan@gmail.com>
2022-07-06 21:20:20 +0800

    Completed Auth controller

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -5,18 +5,28 @@ module Api
     before_action :authorize_request, except: :login
 
     # POST /api/auth/login
-    def login
+    def login # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       @user = User.find_by!(email: params[:email])
 
       if @user&.authenticate(params[:password])
         token = JsonWebToken.encode(user_id: @user.id)
         time = 24.hours.from_now.to_i
-        render json: { token:, exp: time}
-
+        render json: { token:,
+                       exp: Formats::DateTime.jwt(time),
+                       email: @user.email },
+               status: :ok
+      else
+        render json: { error: 'unauthorized' }, status: :unauthorized
+      end
     rescue ActiveRecord::RecordNotFound
-      render json: { error: "User (#{params[:email]}) not found" }, 
+      render json: { error: "User (#{params[:email]}) not found" },
              status: :unprocessable_entity
     end
 
+    private
+
+    def login_params
+      params.permit(:email, :password)
+    end
   end
 end
